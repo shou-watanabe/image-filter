c77b795a73d83d820aea21b82cfd8bea3c821c56
shou-watanabe <watanabeshou1520@gmail.com>
2021-06-06 09:55:13 +0900

    [update]画像表示

---
+++ src/components/organisms/main/FilterPlace.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <ImageDisplayer :image-path="imagePath" /> -->
+    <v-row style="height: 450px" justify="center" align-content="center">
+      <ImageDisplayer :image-path="imagePath" />
+      <v-btn>aaa</v-btn>
+      <ImageDisplayer :image-path="imagePath" />
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ImageDisplayer from "@/components/molecules/ImageDisplayer.vue";
+
+export default Vue.extend({
+  name: "FilterPlace",
+  components: {
+    ImageDisplayer,
+  },
+  data() {
+    return {
+      imagePath:
+        "/Users/watanabeshou/Desktop/image-filter-python/sample_image/fullcolor_sample_gray1.png",
+    };
+  },
+});
+</script>
+
+<style scoped></style>
